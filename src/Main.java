8ccead8fde858929d565a6c40884ea3ba0e2ec95
santiagocanoo <santiagocano922@gmail.com>
2025-10-03 10:08:24 -0500

    implementar clase ArbolBinario con inserción recursiva
    - Se define la clase ArbolBinario con atributo 'raiz'
    - Constructor inicializa la raíz en null
    - Método 'insertar' que delega a 'insertarRec' para agregar nuevos nodos

--- src/Main.java
+++ src/Main.java
@@ -9,3 +9,14 @@ class Nodo {
         izq = der = null;
     }
 }
+class ArbolBinario {
+    Nodo raiz;
+
+    public ArbolBinario() {
+        raiz = null;  // inicializa la raiz vacía
+    }
+
+    // Método para insertar un nuevo valor en el árbol
+    public void insertar(int valor) {
+        raiz = insertarRec(raiz, valor);
+    }
